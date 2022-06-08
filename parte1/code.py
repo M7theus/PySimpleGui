e70c3443ac731e6d9351a6c077861466365249be
M7theus <m7theus_carvalho@protonmail.com>
2022-06-08 12:48:33 -0400

    PySimpleGUI!

--- parte1/code.py
+++ parte1/code.py
@@ -3,5 +3,15 @@ sg.theme('DarkAmber')
 layout = [
     [sg.Text('Some text on Row 1')],
     [sg.Text('Enter something on Row 2'), sg.InputText()],
-    [sg.Button('Ok'), sg.Button('Cancel')]
+    [sg.Button('Ok'), sg.Button('Cancel'),sg.Button('Teste')]
 ]
+
+window = sg.Window('Teste', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    if event == 'Teste':
+        (sg.popup('Conhecendo o PySimpleGUI', 'Esse é o meu primeiro codigo'))
+    print('You entered', values[0])
