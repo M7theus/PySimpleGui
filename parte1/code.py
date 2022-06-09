88b57c6d24dc74c17e071fc86ee6cd8a0c91c9d2
M7theus <m7theus_carvalho@protonmail.com>
2022-06-09 14:24:52 -0400

    .

--- parte1/code.py
+++ parte1/code.py
@@ -1,7 +1,6 @@
 import PySimpleGUI as sg
 sg.theme()
 layout = [
-    [sg.Titlebar('Customized Titlebar Window')],
     [sg.Text('Some text on Row 1')],
     [sg.Text('Enter something on Row 2'), sg.InputText(),sg.FileBrowse('Buscar')],
     [sg.Button('Ok'), sg.Button('Cancel'),sg.Button('Teste')]
@@ -15,4 +14,5 @@ while True:
         break
     if event == 'Teste':
         (sg.popup('Conhecendo o PySimpleGUI', 'Esse é o meu primeiro codigo'))
+        (sg.POPUP_BUTTONS_YES_NO)
     print('You entered', values[0])
