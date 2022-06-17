c590f0e8c4df1e9bab4a86805e61c7e714a9f579
M7theus <m7theus_carvalho@protonmail.com>
2022-06-17 14:43:20 -0400

    Update code.py

--- 10_apps/Converter/cod.py
+++ 10_apps/Converter/cod.py
@@ -1,9 +1,22 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Text'),sg.Spin(['item 1','item 2'])],
-    [sg.Button('Button')],
-    [sg.Input()]
+    [sg.Text('Text',enable_events = True, key ='-Text-'),sg.Spin(['item 1','item 2'])],
+    [sg.Button('Button',key = '-Button1-')],
+    [sg.Input(key = '-Input-')],
+    [sg.Text('Test'), sg.Button('Test Button', key= '-Button2-')]
 ]
 
-sg.Window('Converter',layout).read()
+window = sg.Window('Converter',layout)
+
+while True:
+    event, values = window.read()
+    
+    if event == sg.WIN_CLOSED:
+        break
+    if event == '-Button1-':
+        window['-Text-'].update(values['-Input-'])
+        
+    if event == '-Text-':
+        print('Text was pressed')
+window.close()
