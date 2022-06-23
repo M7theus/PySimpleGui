5ba2e1a87258bc4623acc794ecaf7d57c38168d4
M7theus <m7theus_carvalho@protonmail.com>
2022-06-23 14:04:16 -0400

    Update resposta.py

--- 10_apps/Converter/resposta.py
+++ 10_apps/Converter/resposta.py
@@ -8,4 +8,25 @@ layout = [
     ],
     [sg.Text('Output',key='-Output-')]
           ]
-window = sg.Window('Converter',layout).read()
+window = sg.Window('Converter',layout)
+
+while True:
+    event,values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    if event == '-CONVERT-':
+        input_values = values['-INPUT-']
+        if input_values.isnumeric():
+            match values['-UNITS-']:
+                case 'Km to mile':
+                    output = round(float(input_value)*0.6214,2)
+                    output_string = f'{input_values} km are {output} miles.'
+                case 'Kg to pound':
+                    output = round(float(input_value)*2.20452,2)
+                    output_string = f'{input_value} kg are {output} pounds'
+                case 'sec to min':
+                    output = round(float(input_value) /60,2)
+                    output_string = f'{input_value} seconds are {output} minutes'
+            window['-OUTPUT-'].update(output_string)
+
+window.close()
